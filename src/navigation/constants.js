80f7d1ca11dce22e976963d2d3bd86017064db09
insoojang <insoo2846@gmail.com>
2021-10-01 18:44:14 +0900

    [#59400] Delete Barometer , scan component

--- src/navigation/constants.js
+++ src/navigation/constants.js
@@ -2,9 +2,6 @@ import { i18nt } from '../utils/i18n'
 
 export const SCREEN = {
     Bluetooth: 'Bluetooth',
-    Barometer: 'Barometer',
-    Scan: 'Scan',
     QR: i18nt('title.qr'),
     Main: i18nt('title.main'),
-    BarometerMain: i18nt('title.barometer'),
 }
