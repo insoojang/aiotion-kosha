f0c2392cf3dab6e93781f3cbb697e3807c84cb0b
insoo <2846130>
2021-10-01 18:44:14 +0900

    [#59400] Delete Barometer , scan component

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,16 +1,11 @@
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { SCREEN } from './constants'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import { BluetoothTab } from '../screens/tabs'
 import { NavigationContainer } from '@react-navigation/native'
-import { View, Text } from 'react-native'
 import QrScanner from '../components/QRScanner'
 import { colorSet } from '../styles/colors'
-import BarometerComponent from '../screens/barometer'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import { Tooltip, ListItem } from 'react-native-elements'
-import ScanComponent from '../screens/scan'
 
 const BottomTabStack = createMaterialTopTabNavigator()
 const MainStack = createStackNavigator()
@@ -26,93 +21,23 @@ const MainNavigation = () => {
         </MainStack.Navigator>
     )
 }
-const ListItemComponent = (navi, tooltip) => {
-    return (
-        <View style={{ height: 40 }}>
-            {/*<ListItem*/}
-            {/*    onPress={() => {*/}
-            {/*        tooltip?.toggleTooltip()*/}
-            {/*        navi.navigate(SCREEN.Barometer)*/}
-            {/*    }}*/}
-            {/*    contay*/}
-            {/*>*/}
-            {/*    <Icon name="gauge" size={20} />*/}
-            {/*    <Text>기압계</Text>*/}
-            {/*    <ListItem.Content style={{ height: 18 }} />*/}
-            {/*</ListItem>*/}
-            <ListItem
-                onPress={() => {
-                    tooltip?.toggleTooltip()
-                    navi.navigate(SCREEN.Scan)
-                }}
-            >
-                <Icon name="card-search-outline" size={20} />
-                <Text>Scan</Text>
-                <ListItem.Content style={{ height: 18 }} />
-            </ListItem>
-        </View>
-    )
-}
-
 const RootNavigation = () => {
-    const tooltipRef = useRef(null)
     return (
         <RootStack.Navigator mode="modal">
             <RootStack.Screen
                 name={SCREEN.Main}
                 component={MainNavigation}
-                options={({ navigation }) => ({
+                options={{
                     animationEnabled: false,
                     headerBackTitleVisible: false,
                     headerTintColor: colorSet.normalTextColor,
-                    headerRight: (v) => {
-                        return (
-                            <Tooltip
-                                popover={ListItemComponent(
-                                    navigation,
-                                    tooltipRef.current,
-                                )}
-                                ref={tooltipRef}
-                                width={110}
-                                height={40}
-                                backgroundColor={'white'}
-                                overlayColor={'rgba(0,0,0,.7)'}
-                                withPointer={false}
-                                style={{ marginHorizontal: 15 }}
-                            >
-                                <Icon
-                                    name="dots-vertical"
-                                    size={20}
-                                    style={{ marginHorizontal: 15 }}
-                                />
-                            </Tooltip>
-                        )
-                    },
-                })}
+                }}
             />
             <RootStack.Screen
                 name={SCREEN.QR}
                 component={QrScanner}
                 options={{
                     animationEnabled: false,
-                    headerTintColor: colorSet.normalTextColor,
-                    headerBackTitleVisible: false,
-                }}
-            />
-            <RootStack.Screen
-                name={SCREEN.Barometer}
-                component={BarometerComponent}
-                options={{
-                    // animationEnabled: false,
-                    headerTintColor: colorSet.normalTextColor,
-                    headerBackTitleVisible: false,
-                }}
-            />
-            <RootStack.Screen
-                name={SCREEN.Scan}
-                component={ScanComponent}
-                options={{
-                    // animationEnabled: false,
                     headerTintColor: colorSet.normalTextColor,
                     headerBackTitleVisible: false,
                 }}
--- src/screens/tabs/ScanStyle.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native'
-
-export const SScanView = styled.View`
-    flex: 7.5;
-    padding: 10px 20px;
-    justify-content: space-between;
-`
-SScanView.displayName = 'SScanView'
