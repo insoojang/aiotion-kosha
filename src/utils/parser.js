e218c4846f9d4fe222e65d1872aa8e9cb9a4e4cf
insoojang <insoo2846@gmail.com>
2021-10-20 16:27:07 +0900

    [#59400] 역방향시 알람소리 발생 기능 추가 , 체결레인지 변경

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -14,7 +14,10 @@ const sensorDataParser = (value) => {
     // if (proximity_sensor > 16360) {
     //     return '3'
     // } else
-    if (proximity_sensor > 8000) {
+    if (
+        (proximity_sensor > 9600 && proximity_sensor < 12000) ||
+        (proximity_sensor > 7200 && proximity_sensor < 8300)
+    ) {
         sensorData.push('1')
     } else {
         sensorData.push('0')
